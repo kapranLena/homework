2f65d33194ead006f5a60249d6f7838d6fa548d4
unknown <elena.n.kapran@gmail.com>
2021-08-18 17:29:11 +0200

    Name changed

---
+++ lesson2.py
@@ -0,0 +1,14 @@
+
+
+name = input("Please your name")
+if name == "Lena":
+    print('Hello', name)
+else:
+    print('Hello', name)
+
+""""""
+
+num1 = input('enter num 1')
+num2 = input('enter num 2')
+print(num1+num2)
+
